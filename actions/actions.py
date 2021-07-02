483d13edb379748274998377006b5377cff92610
Anca Lita <27920906+ancalita@users.noreply.github.com>
2021-07-02 13:19:02 +0100

    add knowledge base custom action

--- actions/actions.py
+++ actions/actions.py
@@ -3,9 +3,12 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
+from typing import Text, Dict, List, Any
 
 from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
 from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.interfaces import Action, Tracker
 
 
 class MyKnowledgeBaseAction(ActionQueryKnowledgeBase):
@@ -15,3 +18,31 @@ class MyKnowledgeBaseAction(ActionQueryKnowledgeBase):
             "plant", lambda obj: obj["name"] + " also known as " + obj["botanical_name"] + ": " + obj["description"]
         )
         super().__init__(knowledge_base)
+
+    def object_name_is_present(self, name: Text):
+        for obj in self.knowledge_base.data["plant"]:
+            if obj["name"] == name:
+                return True
+
+        return False
+
+
+class SearchObjects(Action):
+
+    def name(self) -> Text:
+        return "search_objects"
+
+    async def run(self,
+                  dispatcher: CollectingDispatcher,
+                  tracker: Tracker,
+                  domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        knowledge_base_action = MyKnowledgeBaseAction()
+        entity = next(tracker.get_latest_entity_values("plant"), None)
+        if entity:
+            if knowledge_base_action.object_name_is_present(entity):
+                response = "Yes, I can answer questions about this plant."
+            else:
+                response = "I'm sorry, I cannot help this time. " \
+                           "Please ask me to list all the plants I have information on."
+            dispatcher.utter_message(text=response)
+        return []
